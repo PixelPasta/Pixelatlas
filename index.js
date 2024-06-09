0fc5f500a98f3f88baeb19f0fe5155bbf4a975e5
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2024-06-09 13:13:58 +0530

    Fixed app

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const request = require('request')
+const path = require("path")
 const port = process.env.PORT || 5400
 const fetch = require('node-fetch')
+const snoowrap = require('snoowrap')
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
 })
+const { createClient } = require('pexels')
+const client = createClient(process.env.pexel_api_key)
+
 
 let query 
 function capitalizeFirstLetter(string) {
@@ -19,11 +24,33 @@ app.get('/', async (req, res) => {
   res.sendFile(__dirname+'/Pixelatlas.html')
 })
 
+app.set("views", path.join(__dirname, "views"))
+
 app.get('/weather', async (req, res) => {
-   let data = await fetch(`https://pixel-api-production.up.railway.app/data/weather/?location=${req.query.name}`)
-   data = await data.json()
-  if (data.error) return res.render('404')
-   query = req.query.name
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.weather_api_key}&q=${req.query.name}`)
+    let data = await response.json();
+
+    if(!data.location) return res.json({error: "Location not found"})
+   data = {
+    info: {
+        location: data.location.name,
+        country: data.location.country,
+        region: data.location.region,
+    },
+    weather: {
+        temp_c: data.current.temp_c,
+        temp_f: data.current.temp_f,
+        feels_c: data.current.feelslike_c,
+        feels_f: data.current.feelslike_f,
+        condition: data.current.condition.text,
+        wind_kph: data.current.wind_kph,
+        wind_mph: data.current.wind_mph,
+        humidity: data.current.humidity,
+        icon: `https:${data.current.condition.icon}`
+    }
+
+
+}
    res.render('index', 
    {name: capitalizeFirstLetter(req.query.name.toLowerCase()),
     location: data.info.location,
@@ -40,20 +67,21 @@ app.get('/weather', async (req, res) => {
 
 
 app.get('/image.png', async (req, res) => {
-    let response  = await fetch(`https://www.reddit.com/r/EarthPorn/random/.json`)
-    response = await response.json()
+    client.photos.search({query: "nature", page: Math.floor(Math.random() * 100), orientation: 'landscape'}).then(photos => {
+    let image = photos.photos[Math.floor(Math.random() * photos.photos.length)].src.original
     request({
-        url: response[0].data.children[0].data.url,
+        url: image,
         encoding: null
     }, (err, resp, buffer) => {
-        res.set('Content-Type', 'image/jpeg')
+        res.set('Content-Type', 'image/png')
         res.end(buffer)
+    })
     })
    
 })
 
 app.get('/icon.png', async (req, res) => {
-    let response  = await fetch(`https://pixel-api-production.up.railway.app/data/weather/?location=${query}`)
+    let response  = await fetch(`./data/weather/?location=${query}`)
     response = await response.json()
     request({
         url: response.weather.icon,
@@ -63,3 +91,4 @@ app.get('/icon.png', async (req, res) => {
         res.end(buffer)
     })
 })
+
